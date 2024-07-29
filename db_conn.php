deaccc31e331d78c5c5ff476ce613f8e586d11d0
basantbalayar <89195315+basantbalayar@users.noreply.github.com>
2024-07-29 09:34:21 +0545

    Update db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "itinventory";
+$servername = "sql109.infinityfree.com";
+$username = "if0_36992104";
+$password = "M7XUs5oQm7rJq0V";
+$dbname = "if0_36992104_imeevent";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
@@ -11,4 +11,4 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
-// echo "Connected successfully";
+ //echo "Connected successfully";
